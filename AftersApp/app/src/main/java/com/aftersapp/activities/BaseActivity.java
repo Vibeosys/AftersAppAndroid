641a7563d6e09176276623eeb84e825dda1a230d
AkshayVGadhave <akshay@vibeosys.com>
2016-09-16 13:10:38 +0530

    Base Classes and package structure
    Base Classes and package structure

---
+++ AftersApp/app/src/main/java/com/aftersapp/activities/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.aftersapp.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akshay on 16-09-2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ AftersApp/app/src/main/java/com/aftersapp/fragments/BaseFragment.java
@@ -0,0 +1,19 @@
+package com.aftersapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by akshay on 16-09-2016.
+ */
+public class BaseFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
