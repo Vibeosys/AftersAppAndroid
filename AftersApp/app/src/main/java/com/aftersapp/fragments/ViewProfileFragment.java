199aa25cd305d26cadbe3a7ae326d20842e0e67f
AkshayVGadhave <akshay@vibeosys.com>
2016-09-22 11:41:35 +0530

    Image Download and display
    Image Download and display

--- AftersApp/app/src/main/java/com/aftersapp/fragments/ViewProfileFragment.java
+++ AftersApp/app/src/main/java/com/aftersapp/fragments/ViewProfileFragment.java
@@ -1,8 +1,12 @@
 package com.aftersapp.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,19 +17,22 @@ import android.widget.TextView;
 import com.aftersapp.MainActivity;
 import com.aftersapp.R;
 
+import java.io.InputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class ViewProfileFragment extends BaseFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private TextView mUserName,mUserEmailId,mUserDateOfBirth,mUserGender,mUserNotificationStatus;
-
+    private TextView mUserName, mUserEmailId, mUserDateOfBirth, mUserGender, mUserNotificationStatus;
+    private CircleImageView circleView;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private Context mContext;
-
 
 
     public ViewProfileFragment() {
@@ -67,10 +74,41 @@ public class ViewProfileFragment extends BaseFragment {
         mUserName = (TextView) view.findViewById(R.id.userFullNameView);
         mUserEmailId = (TextView) view.findViewById(R.id.userEmailIdView);
         mUserDateOfBirth = (TextView) view.findViewById(R.id.userGendarView);
-        mUserGender =(TextView) view.findViewById(R.id.userDOBView);
+        mUserGender = (TextView) view.findViewById(R.id.userDOBView);
+        circleView = (CircleImageView) view.findViewById(R.id.circleView);
         mUserNotificationStatus = (TextView) view.findViewById(R.id.notificationStatusView);
-
+        DownloadImage downloadImage = new DownloadImage();
+        downloadImage.execute(mSessionManager.getProfImg());
         return view;
     }
 
+
+    // DownloadImage AsyncTask
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+
+            String imageURL = URL[0];
+
+            Bitmap bitmap = null;
+            try {
+                InputStream input = new java.net.URL(imageURL).openStream();
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            circleView.setImageBitmap(result);
+        }
+    }
 }
