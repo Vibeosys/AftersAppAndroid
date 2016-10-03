23419a367398d7366196fafa9ff388f110e9b51e
ShrinivasSH <shrinivas@vibeosys.com>
2016-10-03 15:45:48 +0530

    Edit profile changes for spinner
    Edit profile changes for spinner

--- AftersApp/app/src/main/java/com/aftersapp/fragments/EditMyProfileFragment.java
+++ AftersApp/app/src/main/java/com/aftersapp/fragments/EditMyProfileFragment.java
@@ -54,7 +54,7 @@ import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class EditMyProfileFragment extends BaseFragment implements View.OnClickListener,ServerSyncManager.OnSuccessResultReceived, ServerSyncManager.OnErrorResultReceived  {
+public class EditMyProfileFragment extends BaseFragment implements View.OnClickListener, ServerSyncManager.OnSuccessResultReceived, ServerSyncManager.OnErrorResultReceived {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,20 +65,21 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
     private String mParam2;
 
 
-    private EditText mDateOfBirth,mUserName,mUserEmailId;
+    private EditText mDateOfBirth, mUserName, mUserEmailId;
     Calendar myCalendar;
     private Spinner mSpinner;
-    private Button mSaveUserProfile,mCancelUserProfile;
+    private Button mSaveUserProfile, mCancelUserProfile;
     private Switch mUserNotoficationSwitch;
     private static final String HOME_FRAGMENT_CANCEL_PROFILE = "home";
     private String mSpinnerSelectionVal;
     private long mSwitchValNotification;
     private TextView mUserNameFirst;
     private CircleImageView mEditUserImage;
-    private static final String HOME_FRAGMENT_EDIT_PROFILE="home";
-    String userEmail,userFullName,userDate;
+    private static final String HOME_FRAGMENT_EDIT_PROFILE = "home";
+    String userEmail, userFullName, userDate;
     private View mView;
     int NotifyVal;
+    int mPosition;
     ProgressDialog dialog;
 
 
@@ -92,21 +93,20 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_edit_my_profile, container, false);
-        mDateOfBirth =(EditText) rootView.findViewById(R.id.userDOB);
-        mSpinner =(Spinner) rootView.findViewById(R.id.spinner);
+        mDateOfBirth = (EditText) rootView.findViewById(R.id.userDOB);
+        mSpinner = (Spinner) rootView.findViewById(R.id.spinner);
         mUserName = (EditText) rootView.findViewById(R.id.userFullName);
         mUserEmailId = (EditText) rootView.findViewById(R.id.userEmailId);
         mSaveUserProfile = (Button) rootView.findViewById(R.id.saveProfile);
         mCancelUserProfile = (Button) rootView.findViewById(R.id.cancelProfile);
-        mUserNotoficationSwitch =(Switch) rootView.findViewById(R.id.switch1);
+        mUserNotoficationSwitch = (Switch) rootView.findViewById(R.id.switch1);
         mUserNameFirst = (TextView) rootView.findViewById(R.id.userName);
-        mEditUserImage =(CircleImageView) rootView.findViewById(R.id.circleView);
+        mEditUserImage = (CircleImageView) rootView.findViewById(R.id.circleView);
 
-        mView =rootView.findViewById(R.id.firstView);
+        mView = rootView.findViewById(R.id.firstView);
         mSaveUserProfile.setOnClickListener(this);
         mCancelUserProfile.setOnClickListener(this);
         mServerSyncManager.setOnStringErrorReceived(this);
@@ -116,49 +116,48 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
         dialog.setMessage("Loading. Please wait...");
         dialog.setIndeterminate(true);
         dialog.setCanceledOnTouchOutside(false);
-       // dialog.show();
-        mUserName.setText(""+mSessionManager.getName());
-        mUserNameFirst.setText(""+mSessionManager.getName());
-        mUserEmailId.setText(""+mSessionManager.getEmail2());
+        // dialog.show();
+        mUserName.setText("" + mSessionManager.getName());
+        mUserNameFirst.setText("" + mSessionManager.getName());
+        mUserEmailId.setText("" + mSessionManager.getEmail2());
 
         DownloadImage downloadImage = new DownloadImage();
         downloadImage.execute(mSessionManager.getProfImg());
 
-        if(!TextUtils.isEmpty(mSessionManager.getDob()))
-            mDateOfBirth.setText(""+mSessionManager.getDob());
+        if (!TextUtils.isEmpty(mSessionManager.getDob()))
+            mDateOfBirth.setText("" + mSessionManager.getDob());
         else
             mDateOfBirth.setText("Click here to enter birth date");
 
 
-        mSwitchValNotification =mSessionManager.getEmailNotify();
-        mSpinnerSelectionVal=mSessionManager.getGender();
-        List<String> spineerData =  new ArrayList<>();
+        mSwitchValNotification = mSessionManager.getEmailNotify();
+        mSpinnerSelectionVal = mSessionManager.getGender();
+        List<String> spineerData = new ArrayList<>();
+        spineerData.add("");
         spineerData.add("Male");
         spineerData.add("Female");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
-                (getActivity(), android.R.layout.simple_spinner_item,spineerData);
+                (getActivity(), android.R.layout.simple_spinner_item, spineerData);
 
         dataAdapter.setDropDownViewResource
                 (android.R.layout.simple_spinner_dropdown_item);
 
         mSpinner.setAdapter(dataAdapter);
 
-        if(!TextUtils.isEmpty(mSpinnerSelectionVal))
-        {
-           int spineerPosition = dataAdapter.getPosition(mSpinnerSelectionVal);
+        if (!TextUtils.isEmpty(mSpinnerSelectionVal)) {
+            int spineerPosition = dataAdapter.getPosition(mSpinnerSelectionVal);
             mSpinner.setSelection(spineerPosition);
-        }
-        else if(TextUtils.isEmpty(mSpinnerSelectionVal))
-        {
-            Log.d("TAG","TAG");
-            Log.d("TAG","TAG");
+        } else if (TextUtils.isEmpty(mSpinnerSelectionVal)) {
+            Log.d("TAG", "TAG");
+            Log.d("TAG", "TAG");
         }
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 mSpinnerSelectionVal = mSpinner.getItemAtPosition(position).toString();
-                Log.d("TAG","TAG");
-                Log.d("TAG","TAG");
+                mPosition = position;
+                Log.d("TAG", "TAG");
+                Log.d("TAG", "TAG");
             }
 
             @Override
@@ -166,11 +165,9 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
 
             }
         });
-        if(mSwitchValNotification==1)
-        {
+        if (mSwitchValNotification == 1) {
             mUserNotoficationSwitch.setChecked(true);
-        }else if(mSwitchValNotification==0)
-        {
+        } else if (mSwitchValNotification == 0) {
             mUserNotoficationSwitch.setChecked(false);
         }
         mDateOfBirth.setInputType(InputType.TYPE_NULL);
@@ -179,7 +176,7 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 myCalendar = Calendar.getInstance();
-                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     new DatePickerDialog(getActivity(), date, myCalendar
                             .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                             myCalendar.get(Calendar.DAY_OF_MONTH)).show();
@@ -189,23 +186,19 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
         });
 
         mUserNotoficationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-           @Override
-           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-               if(mUserNotoficationSwitch.isChecked())
-               {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (mUserNotoficationSwitch.isChecked()) {
 
-                   mSwitchValNotification=1;
-               }
-               else
-               {
+                    mSwitchValNotification = 1;
+                } else {
 
-                   mSwitchValNotification=0;
-               }
-           }
-       });
+                    mSwitchValNotification = 0;
+                }
+            }
+        });
         return rootView;
     }
-
 
 
     DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -219,25 +212,24 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
             updateLabel();
         }
     };
+
     private void updateLabel() {
-      //  String myFormat = "dd-MMM-yyyy"; //In which you need put here
+        //  String myFormat = "dd-MMM-yyyy"; //In which you need put here
         //SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         DateUtils dateUtils = new DateUtils();
         mDateOfBirth.setText(dateUtils.getLocalDateInFormat(myCalendar.getTime()));
-       // mDateOfBirth.setText(sdf.format(myCalendar.getTime()));
+        // mDateOfBirth.setText(sdf.format(myCalendar.getTime()));
         mDateOfBirth.setError(null);
     }
 
 
     @Override
     public void onClick(View v) {
-        int id =v.getId();
-        switch(id)
-        {
+        int id = v.getId();
+        switch (id) {
             case R.id.saveProfile:
-                boolean returnVal =callToValidation();
-                if(returnVal==true)
-                {
+                boolean returnVal = callToValidation();
+                if (returnVal == true) {
                     dialog.show();
                     CallToWebServices();
                 }
@@ -253,59 +245,59 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
 
     private void CallToWebServices() {
 
-         userEmail = mUserEmailId.getText().toString();
-         userFullName = mUserName.getText().toString();
-         userDate = mDateOfBirth.getText().toString();
+        userEmail = mUserEmailId.getText().toString();
+        userFullName = mUserName.getText().toString();
+        userDate = mDateOfBirth.getText().toString();
         String convertedDate = "";
-        DateUtils dateUtils =new DateUtils();
+        DateUtils dateUtils = new DateUtils();
         convertedDate = dateUtils.convertFbDateToSwedish(userDate);
-         NotifyVal =(int) mSwitchValNotification;
-        UpdateProfileDTO updateProfileDTO = new UpdateProfileDTO(mSessionManager.getUserId(),userFullName,mSessionManager.getEmail(),
-                userEmail,"123456789",mSpinnerSelectionVal,mSessionManager.getProfImg(),convertedDate,mSessionManager.getToken(),NotifyVal);
+        NotifyVal = (int) mSwitchValNotification;
+        UpdateProfileDTO updateProfileDTO = new UpdateProfileDTO(mSessionManager.getUserId(), userFullName, mSessionManager.getEmail(),
+                userEmail, "123456789", mSpinnerSelectionVal, mSessionManager.getProfImg(), convertedDate, mSessionManager.getToken(), NotifyVal);
 
         String url = mSessionManager.getEditProfileUrl();
         Gson gson = new Gson();
-        String serlize =gson.toJson(updateProfileDTO);
+        String serlize = gson.toJson(updateProfileDTO);
         BaseRequestDTO baseRequestDTO = new BaseRequestDTO();
         baseRequestDTO.setData(serlize);
         mServerSyncManager.uploadDataToServer(ServerRequestConstants.REQUEST_EDIT_PROFILE,
                 mSessionManager.getEditProfileUrl(), baseRequestDTO);
-        Log.d("TAG","TAG");
-        Log.d("TAG","TAG");
+        Log.d("TAG", "TAG");
+        Log.d("TAG", "TAG");
 
     }
 
     private boolean callToValidation() {
-            userDate = mDateOfBirth.getText().toString();
-            if(TextUtils.isEmpty(mUserName.getText().toString().trim()))
-            {
-                mUserName.requestFocus();
-                mUserName.setError("Please enter userName");
+        userDate = mDateOfBirth.getText().toString();
+        if (TextUtils.isEmpty(mUserName.getText().toString().trim())) {
+            mUserName.requestFocus();
+            mUserName.setError("Please enter userName");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(mUserEmailId.getText().toString().trim())) {
+            mUserEmailId.requestFocus();
+            mUserEmailId.setError("Please enter email Id");
+            return false;
+        } else if (mUserEmailId.getText().toString().trim().length() != 0) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(mUserEmailId.getText().toString()).matches()) {
+                mUserEmailId.requestFocus();
+                mUserEmailId.setError("Invalid email Id");
                 return false;
             }
-
-           if(TextUtils.isEmpty(mUserEmailId.getText().toString().trim()))
-            {
-                mUserEmailId.requestFocus();
-                mUserEmailId.setError("Please enter email Id");
-                return false;
-            }else if(mUserEmailId.getText().toString().trim().length()!=0)
-           {
-               if(!Patterns.EMAIL_ADDRESS.matcher(mUserEmailId.getText().toString()).matches())
-               {
-                   mUserEmailId.requestFocus();
-                   mUserEmailId.setError("Invalid email Id");
-                   return false;
-               }
-           }if(userDate.equals("Click here to enter birth date"))
-             {
-                 mUserEmailId.requestFocus();
-                 mDateOfBirth.setError("Please select Birth date");
-                 Toast toast = Toast.makeText(getContext(),"Please select Birth date",Toast.LENGTH_LONG);
-                 toast.setGravity(Gravity.CENTER,0,0);
-                 toast.show();
-                 return false;
-             }
+        }
+        if (userDate.equals("Click here to enter birth date")) {
+            mUserEmailId.requestFocus();
+            mDateOfBirth.setError("Please select date of birth");
+            Toast toast = Toast.makeText(getContext(), "Please select date of birth", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+            return false;
+        }
+        if (mPosition == 0) {
+            createAlertDialog("Afters App", "Please select gender");
+            return false;
+        }
         return true;
     }
 
@@ -335,8 +327,8 @@ public class EditMyProfileFragment extends BaseFragment implements View.OnClickL
         switch (requestToken) {
             case ServerRequestConstants.REQUEST_EDIT_PROFILE:
 
-                Toast toast = Toast.makeText(getContext(),"Profile Updated Successfully",Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER,0,0);
+                Toast toast = Toast.makeText(getContext(), "Profile Updated Successfully", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
                 toast.show();
                 dialog.cancel();
                 RegisterResponseData registerResponseData = RegisterResponseData.deserializeJson(data);
